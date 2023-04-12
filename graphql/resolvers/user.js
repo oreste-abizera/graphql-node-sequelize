bda6e33648a860f9d71aba3db8367a5170760124
oreste-abizera <oresteabizera11@gmail.com>
2023-04-12 17:56:17 +0200

    feat: handle authentication headers

--- graphql/resolvers/user.js
+++ graphql/resolvers/user.js
@@ -2,22 +2,22 @@ const jwt = require("jsonwebtoken");
 const bcrypt = require("bcryptjs");
 const { AuthenticationError } = require("apollo-server-express");
 
-const { db } = require("../../database/models");
+const { User } = require("../../database/models");
 
 module.exports = {
   Mutation: {
     async register(root, args, context) {
       const { name, email, password } = args.input;
-      const user = await db.User.findOne({ where: { email } });
+      const user = await User.findOne({ where: { email } });
       if (user) {
         throw new AuthenticationError("Email already exists");
       }
-      return db.User.create({ name, email, password });
+      return User.create({ name, email, password });
     },
 
     async login(root, { input }, context) {
       const { email, password } = input;
-      const user = await db.User.findOne({ where: { email } });
+      const user = await User.findOne({ where: { email } });
       if (user && bcrypt.compareSync(password, user.password)) {
         const token = jwt.sign({ id: user.id }, "mySecret");
         return { ...user.toJSON(), token };
