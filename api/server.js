aa6dd5133a97c7c2fab86078959381c6e809313b
oreste-abizera <oresteabizera11@gmail.com>
2023-04-13 12:27:14 +0200

    feat: embedd playground

--- api/server.js
+++ api/server.js
@@ -5,6 +5,9 @@ const cors = require("cors");
 const typeDefs = require("../graphql/schemas");
 const resolvers = require("../graphql/resolvers");
 const context = require("../graphql/context");
+const {
+  ApolloServerPluginLandingPageLocalDefault,
+} = require("apollo-server-core");
 
 async function startServer() {
   const app = express();
@@ -21,6 +24,7 @@ async function startServer() {
         "schema.polling.enable": false,
       },
     },
+    plugins: [ApolloServerPluginLandingPageLocalDefault({ embed: true })],
   });
 
   await apolloServer.start();
